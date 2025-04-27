ba70d2216e293f62df47a5e87a79d17376306cf3
Tëma Bolshakov <either.free@gmail.com>
2025-04-27 12:18:45 +0200

    Interface revamp

--- lib/stoplight_admin/lights_repository/light.rb
+++ lib/stoplight_admin/lights_repository/light.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "securerandom"
+
 module StoplightAdmin
   class LightsRepository
     class Light
@@ -8,6 +10,10 @@ module StoplightAdmin
         YELLOW = Stoplight::Color::YELLOW,
         RED = Stoplight::Color::RED
       ].freeze
+
+      # @!attribute id
+      #   @return [String]
+      attr_reader :id
 
       # @!attribute name
       #   @return [String]
@@ -30,10 +36,15 @@ module StoplightAdmin
       # @param state [String]
       # @param failures [<Stoplight::Failure>]
       def initialize(name:, color:, state:, failures:)
+        @id = SecureRandom.uuid
         @name = name
         @color = color
         @state = state
         @failures = failures
+      end
+
+      def latest_failure
+        failures.first
       end
 
       # @return [Boolean]
@@ -58,6 +69,81 @@ module StoplightAdmin
       # @return [Array]
       def default_sort_key
         [-COLORS.index(color), name]
+      end
+
+      # @return [String, nil]
+      def last_check_in_words
+        last_failure_time = latest_failure&.time
+        return unless last_failure_time
+
+        time_difference = Time.now - last_failure_time
+        if time_difference < 60
+          "#{time_difference.to_i}s ago"
+        elsif time_difference < 3600
+          "#{(time_difference / 60).to_i}m ago"
+        else
+          "#{(time_difference / 3600).to_i}h ago"
+        end
+      end
+
+      # @return [String]
+      def description_title
+        case color
+        when RED
+          if locked? && failures.empty?
+            "Locked Open"
+          else
+            "Last Error"
+          end
+        when YELLOW
+          "Testing Recovery"
+        when GREEN
+          if locked?
+            "Forced Healthy"
+          else
+            "Healthy"
+          end
+        end
+      end
+
+      # @return [String]
+      def description_message
+        case color
+        when RED
+          if locked? && failures.empty?
+            "Circuit manually locked open"
+          else
+            "#{latest_failure.error_class}: #{latest_failure.error_message}"
+          end
+        when YELLOW
+          "#{latest_failure.error_class}: #{latest_failure.error_message}"
+        when GREEN
+          if locked?
+            "Circuit manually locked closed"
+          else
+            "No recent errors"
+          end
+        end
+      end
+
+      # @return [String]
+      def description_comment
+        case color
+        when RED
+          if locked?
+            "Override active - all requests blocked"
+          else
+            "Will attempt recovery after cooling period"
+          end
+        when YELLOW
+          "Allowing limited test traffic (0 of 1 requests)"
+        when GREEN
+          if locked?
+            "Override active - all requests processed"
+          else
+            "Operating normally"
+          end
+        end
       end
     end
   end
